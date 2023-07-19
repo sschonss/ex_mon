540cb5c4d36ef3cd3450a381fbfd4b6b33af8de0
sschonss <schonsluuiz@gmail.com>
2023-07-19 20:45:38 -0300

    Add Pipe

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -2,13 +2,16 @@ defmodule ExMon do
   
   alias ExMon.{Game, Player}
 
+  @computer_name "Robot"
+
   def create_player(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
   end
 
-  def start_game(player) do
-    computer = create_player("Robot", :punch, :kick, :heal)
-    Game.start(computer, player)
+  def start_game(player) do 
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
   end
 
 end
