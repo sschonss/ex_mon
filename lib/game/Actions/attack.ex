2ef0e9ce140ffed613e4b911c0bf83ce741f140b
sschonss <schonsluuiz@gmail.com>
2023-07-19 21:34:48 -0300

    Create Calculate Power

--- lib/game/Actions/attack.ex
+++ lib/game/Actions/attack.ex
@@ -4,8 +4,12 @@ defmodule ExMon.Game.Actions.Attack do
     @move_rnd_power 10..35
 
     def attack_opponent(opponent, move) do
-        opponent
+        damage = calculate_power(move)
         
     end
+
+    defp calculate_power(:move_avg), do: Enum.random(@move_avg_power)
+    defp calculate_power(:move_rnd), do: Enum.random(@move_rnd_power)
+    
 
 end
