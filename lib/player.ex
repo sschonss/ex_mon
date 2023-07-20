21b3a1269e3bef61704fefc843030e4a19fbf129
sschonss <schonsluuiz@gmail.com>
2023-07-19 21:16:09 -0300

    Refactory Player Moves

--- lib/player.ex
+++ lib/player.ex
@@ -1,6 +1,6 @@
 defmodule ExMon.Player do
   
-    @required_keys [:life, :move_avg, :move_heal, :move_rnd, :name]
+    @required_keys [:life, :moves, :name]
     @max_life 100
 
     @enforce_keys @required_keys
@@ -9,9 +9,11 @@ defmodule ExMon.Player do
     def build(name, move_rnd, move_avg, move_heal) do
         %ExMon.Player{
             life: @max_life,
-            move_avg: move_avg,
-            move_heal: move_heal,
-            move_rnd: move_rnd,
+            moves: %{    
+                move_avg: move_avg,
+                move_heal: move_heal,
+                move_rnd: move_rnd
+            },
             name: name,
         }
     end
