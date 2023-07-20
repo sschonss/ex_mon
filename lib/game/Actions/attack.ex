22c85d094bb9a99553015426109189340cbc1060
sschonss <schonsluuiz@gmail.com>
2023-07-19 21:30:31 -0300

    Create Module Attack

---
+++ lib/game/Actions/attack.ex
@@ -0,0 +1,11 @@
+defmodule ExMon.Game.Actions.Attack do
+
+    @move_avg_power 18..25
+    @move_rnd_power 10..35
+
+    def attack_opponent(opponent, move) do
+        opponent
+        
+    end
+
+end
